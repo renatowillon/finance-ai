5b3954e698af2d3d8c8178feb5e5e85baafa21d2
Renato Willon <renatowillon@hotmail.com>
2025-02-01 15:43:21 -0300

    fix: upsert transaction

--- app/_actions/upsert-transaction/index.ts
+++ app/_actions/upsert-transaction/index.ts
@@ -27,11 +27,11 @@ export const upsertTransaction = async (params: upsertTransactionParams) => {
     throw new Error("Não autorizado!, realize o login.");
   }
   await db.transaction.upsert({
-    where: {
-      id: params.id,
-    },
     update: { ...params, userId },
     create: { ...params, userId },
+    where: {
+      id: params.id ?? "",
+    },
   });
   revalidatePath("/transaction");
 };
