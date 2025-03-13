0f2aa8d600b033e3b84dcc67c7104d5f993f6934
“renatowillon” <renatowillon@hotmail.com>
2025-03-13 15:48:22 -0300

    fix: correção em scrool bar e icon transferencia

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -19,7 +19,7 @@ const TransactionsPage = async () => {
   return (
     <>
       <Navbar />
-      <div className="space-y-6 overflow-hidden p-6">
+      <div className="space-y-6 p-6">
         {/* titulo e botão */}
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
