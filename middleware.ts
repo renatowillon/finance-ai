91e4bd5f9dadbf49812052f5c56322440c8362b5
“renatowillon” <renatowillon@hotmail.com>
2025-07-30 16:28:59 -0300

    fix: correção de middleware

--- middleware.ts
+++ middleware.ts
@@ -19,5 +19,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|logo-wfinance.png).*)"],
 };
