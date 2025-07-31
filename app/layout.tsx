490aceed67071615d54dfdf2df2371e7e9879e33
“renatowillon” <renatowillon@hotmail.com>
2025-07-31 11:37:31 -0300

    fix: manifest logos

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="manifest" href="/manifest.json" />
         <meta name="apple-mobile-web-app-title" content="wFinance" />
       </head>
       <body
