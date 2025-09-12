13153850e415f778b78fc5934bae81b2b6e2a619
“renatowillon” <renatowillon@hotmail.com>
2025-09-12 17:27:08 -0300

    feat: inclusão de titulos em Bancos e Investimentos

--- app/bancos/page.tsx
+++ app/bancos/page.tsx
@@ -1,4 +1,18 @@
-const Bancos = () => {
-  return <div>BANCOS</div>;
+import AddTransactionButton from "../_components/add-transactions-button";
+import { canUserAddTransaction } from "../_data/can-user-add-transaction";
+
+const Bancos = async () => {
+  const usuarioPodeAdicionarTransacao = await canUserAddTransaction();
+  return (
+    <div className="space-y-6 p-6">
+      {/* titulo e botão */}
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Bancos</h1>
+        <AddTransactionButton
+          usuarioPodeAdicionarTransacao={usuarioPodeAdicionarTransacao}
+        />
+      </div>
+    </div>
+  );
 };
 export default Bancos;
--- app/investimentos/page.tsx
+++ app/investimentos/page.tsx
@@ -1,4 +1,18 @@
-const Investimentos = () => {
-  return <div>INVESTIMENTOS</div>;
+import AddTransactionButton from "../_components/add-transactions-button";
+import { canUserAddTransaction } from "../_data/can-user-add-transaction";
+
+const Investimentos = async () => {
+  const usuarioPodeAdicionarTransacao = await canUserAddTransaction();
+  return (
+    <div className="space-y-6 p-6">
+      {/* titulo e botão */}
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Investimentos</h1>
+        <AddTransactionButton
+          usuarioPodeAdicionarTransacao={usuarioPodeAdicionarTransacao}
+        />
+      </div>
+    </div>
+  );
 };
 export default Investimentos;
