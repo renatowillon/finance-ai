d4e3765ad5d11c463b6b1eb5c2154ea4e50b566e
“renatowillon” <renatowillon@hotmail.com>
2025-10-06 17:14:36 -0300

    feat: ajuste de card de categoria WF-7

--- app/_components/categorias/cardCategoria.tsx
+++ app/_components/categorias/cardCategoria.tsx
@@ -20,21 +20,25 @@ export const CardCategoria = ({ dataCategoria, editCategoria }: Props) => {
             <span className="rounded-lg p-2">
               <Tags />
             </span>
-            <div className={`flex gap-3`}>
-              <p className="text-lg font-bold">{dataCategoria.nome}</p>
+            <div className={`w-full`}>
+              <p className="w-full truncate text-sm font-bold">
+                {dataCategoria.nome}
+              </p>
               <Badge
-                className={`${ROTULO_TIPO_CATEGORIA[dataCategoria.tipo] === "Despesas" ? "bg-red-500 hover:bg-red-600" : "bg-green-500 hover:bg-green-600"}`}
+                className={`w-20 items-center justify-center ${ROTULO_TIPO_CATEGORIA[dataCategoria.tipo] === "Despesas" ? "bg-red-500 hover:bg-red-600" : "bg-green-500 hover:bg-green-600"}`}
               >
                 {ROTULO_TIPO_CATEGORIA[dataCategoria.tipo]}
               </Badge>
             </div>
           </div>
-          <Button
-            onClick={() => editCategoria(dataCategoria)}
-            className="bg-secondary/25 hover:bg-secondary"
-          >
-            <EllipsisVertical />
-          </Button>
+          <div className="flex items-center gap-3">
+            <Button
+              onClick={() => editCategoria(dataCategoria)}
+              className="bg-secondary/25 hover:bg-secondary"
+            >
+              <EllipsisVertical />
+            </Button>
+          </div>
         </div>
       </Card>
     </>
--- app/categorias/page.tsx
+++ app/categorias/page.tsx
@@ -79,7 +79,7 @@ const Categorias = () => {
           onClick={() => setAbrirFormCategoria(true)}
         />
       )}
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-3 lg:grid-cols-4">
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
         {data?.map((categoria: TypeCategoria) => (
           <CardCategoria
             key={categoria.id}
